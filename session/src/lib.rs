1d98afe59f924f9f60d6d13d77b732e5df0e012b
Daniel Cheng <zetafunction@gmail.com>
2025-08-14 22:56:38 -0700

    Add golden tests for session serialization/deserialization

--- session/src/lib.rs
+++ session/src/lib.rs
@@ -5,7 +5,7 @@ pub mod torrent {
     pub use self::current::Session;
     pub use self::ver_fa1b6f as current;
 
-    #[derive(Serialize, Deserialize, Clone)]
+    #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
     pub struct Bitfield {
         pub len: u64,
         pub data: Box<[u8]>,
@@ -40,7 +40,7 @@ pub mod torrent {
 
         use std::path::PathBuf;
 
-        #[derive(Serialize, Deserialize)]
+        #[derive(Deserialize, Debug, PartialEq, Serialize)]
         pub struct Session {
             pub info: Info,
             pub pieces: Bitfield,
@@ -56,7 +56,7 @@ pub mod torrent {
             pub trackers: Vec<String>,
         }
 
-        #[derive(Clone, Serialize, Deserialize)]
+        #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
         pub struct Info {
             pub name: String,
             pub announce: Option<String>,
@@ -72,13 +72,13 @@ pub mod torrent {
             pub piece_idx: Vec<(usize, u64)>,
         }
 
-        #[derive(Serialize, Deserialize, Clone, Debug)]
+        #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
         pub struct File {
             pub path: PathBuf,
             pub length: u64,
         }
 
-        #[derive(Clone, Debug, Serialize, Deserialize)]
+        #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
         pub struct Status {
             pub paused: bool,
             pub validating: bool,
@@ -86,7 +86,7 @@ pub mod torrent {
             pub state: StatusState,
         }
 
-        #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+        #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
         pub enum StatusState {
             Magnet,
             // Torrent has not acquired all pieces
@@ -383,5 +383,100 @@ pub mod torrent {
                 .migrate()
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::DateTime;
+    use std::path::PathBuf;
+
+    use super::torrent::current::{File, Info, Session, Status, StatusState};
+    use super::torrent::Bitfield;
+
+    fn session_instance() -> Session {
+        Session {
+            info: Info {
+                name: "Hello world!".to_string(),
+                announce: Some("announce".to_string()),
+                creator: Some("creator".to_string()),
+                comment: Some("comment".to_string()),
+                piece_len: 1048576,
+                total_len: 2 * 1048576,
+                hashes: vec![
+                    b"\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29".to_vec(),
+                    b"\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39".to_vec(),
+                ],
+                hash: *b"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19",
+                files: vec![
+                    File {
+                        path: PathBuf::from("file1"),
+                        length: 1024,
+                    },
+                    File {
+                        path: PathBuf::from("file2"),
+                        length: 2 * 1048576 - 1024,
+                    },
+                ],
+                private: true,
+                be_name: None,
+                piece_idx: vec![(0, 1), (1, 0)],
+            },
+            pieces: Bitfield {
+                len: 2,
+                data: Box::new([3]),
+            },
+            uploaded: 7777777,
+            downloaded: 88888888,
+            status: Status {
+                paused: false,
+                validating: true,
+                error: Some("an error".to_string()),
+                state: StatusState::Complete,
+            },
+            path: Some("/tmp".to_string()),
+            priority: 100,
+            priorities: vec![],
+            created: DateTime::from_timestamp(946684799, 0).unwrap(),
+            throttle_ul: Some(64 * 1024 * 1024),
+            throttle_dl: None,
+            trackers: vec!["https://example.com:1234/tracker".to_string()],
+        }
+    }
+
+    const SESSION_SERIALIZATION: &[u8] = &[
+        12, 0, 0, 0, 0, 0, 0, 0, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33, 1, 8, 0,
+        0, 0, 0, 0, 0, 0, 97, 110, 110, 111, 117, 110, 99, 101, 1, 7, 0, 0, 0, 0, 0, 0, 0, 99, 114,
+        101, 97, 116, 111, 114, 1, 7, 0, 0, 0, 0, 0, 0, 0, 99, 111, 109, 109, 101, 110, 116, 0, 0,
+        16, 0, 0, 0, 32, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 32, 33,
+        34, 35, 36, 37, 38, 39, 40, 41, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 20, 0, 0, 0, 0, 0,
+        0, 0, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 16,
+        17, 18, 19, 20, 21, 22, 23, 24, 25, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 2, 0, 0, 0, 0,
+        0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 102, 105, 108, 101, 49, 0, 4, 0, 0, 0, 0, 0, 0, 5, 0, 0,
+        0, 0, 0, 0, 0, 102, 105, 108, 101, 50, 0, 252, 31, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3, 241, 173, 118, 0, 0, 0, 0,
+        0, 56, 86, 76, 5, 0, 0, 0, 0, 0, 1, 1, 8, 0, 0, 0, 0, 0, 0, 0, 97, 110, 32, 101, 114, 114,
+        111, 114, 2, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 47, 116, 109, 112, 100, 0, 0, 0, 0, 0, 0,
+        0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 49, 57, 57, 57, 45, 49, 50, 45, 51, 49, 84, 50, 51, 58, 53,
+        57, 58, 53, 57, 90, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0,
+        0, 0, 0, 104, 116, 116, 112, 115, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99,
+        111, 109, 58, 49, 50, 51, 52, 47, 116, 114, 97, 99, 107, 101, 114,
+    ];
+
+    #[test]
+    fn stable_deserialize() {
+        assert_eq!(
+            bincode::serialize(&session_instance()).unwrap(),
+            SESSION_SERIALIZATION
+        );
+    }
+
+    #[test]
+    fn stable_serialize() {
+        assert_eq!(
+            super::torrent::load(&SESSION_SERIALIZATION).unwrap(),
+            session_instance()
+        );
     }
 }
