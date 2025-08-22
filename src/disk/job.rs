zetafunction/synapse

commit abcb6507d87bedc63bc721ce48d3fb8237d4f902

Separate immutable state out of session state
- The immutable state is serialized into a <torrenthash>.info file.
- The mutable session state is still serialized into <torrenthash>, as
  before, with no extension.
- Session migration handles  deserializing a torrent from one (legacy
  session data only) or two (session data + info data) blobs.
- Deserializing a torrent that requires migration promptly re-serializes
  the updated version to the disk. While this could be lazily done on
  demand instead, likely failure modes would result in data loss.
- For torrents with a lot of files or many pieces, this can result in a
  100x+ reduction of bytes written for each session update.

src/disk/job.rs
@@ -49,6 +49,7 @@ pub enum Request {
         tid: usize,
         data: Vec<u8>,
         hash: [u8; 20],
+        extension: Option<&'static str>,
     },
     Delete {
         tid: usize,
@@ -138,8 +139,19 @@ impl Request {
         }
     }
 
-    pub fn serialize(tid: usize, data: Vec<u8>, hash: [u8; 20]) -> Request {
-        Request::Serialize { tid, data, hash }
+    pub fn serialize(
+        tid: usize,
+        data: Vec<u8>,
+        hash: [u8; 20],
+        extension: Option<&'static str>,
+    ) -> Request {
+        assert!(extension.is_none_or(|e| e.starts_with('.')));
+        Request::Serialize {
+            tid,
+            data,
+            hash,
+            extension,
+        }
     }
 
     pub fn validate(tid: usize, info: Arc<Info>, path: Option<String>) -> Request {
@@ -401,9 +413,15 @@ impl Request {
                 }
                 return Ok(JobRes::Resp(Response::moved(tid, to)));
             }
-            Request::Serialize { data, hash, .. } => {
+            Request::Serialize {
+                data,
+                hash,
+                extension,
+                ..
+            } => {
+                let extension = extension.unwrap_or("");
                 let temp = tpb.get(sd);
-                temp.push(hash_to_id(&hash) + ".temp");
+                temp.push(hash_to_id(&hash) + extension + ".temp");
                 let mut f = fs::OpenOptions::new()
                     .create(true)
                     .truncate(true)
@@ -411,7 +429,7 @@ impl Request {
                     .open(&temp)?;
                 f.write_all(&data)?;
                 let actual = tpb2.get(sd);
-                actual.push(hash_to_id(&hash));
+                actual.push(hash_to_id(&hash) + extension);
                 fs::rename(temp, actual)?;
             }
             Request::Delete {

src/control/job.rs
@@ -35,7 +35,7 @@ pub struct SessionUpdate;
 impl<T: cio::CIO> Job<T> for SessionUpdate {
     fn update(&mut self, torrents: &mut UHashMap<Torrent<T>>) {
         for (_, torrent) in torrents.iter_mut() {
-            torrent.serialize_if_dirty();
+            torrent.serialize_session_if_dirty();
         }
     }
 }

session/src/lib.rs
@@ -2,8 +2,8 @@
 extern crate serde_derive;
 
 pub mod torrent {
-    pub use self::current::Session;
-    pub use self::ver_fa1b6f as current;
+    pub use self::current::Torrent;
+    pub use self::ver_bfbf28 as current;
 
     #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
     pub struct Bitfield {
@@ -11,19 +11,97 @@ pub mod torrent {
         pub data: Box<[u8]>,
     }
 
-    pub fn load(data: &[u8]) -> Option<Session> {
-        if let Ok(m) = bincode::deserialize::<ver_fa1b6f::Session>(data) {
-            Some(m)
-        } else if let Ok(m) = bincode::deserialize::<ver_6e27af::Session>(data) {
-            Some(m.migrate())
-        } else if let Ok(m) = bincode::deserialize::<ver_249b1b::Session>(data) {
-            Some(m.migrate())
-        } else if let Ok(m) = bincode::deserialize::<ver_5f166d::Session>(data) {
-            Some(m.migrate())
-        } else if let Ok(m) = bincode::deserialize::<ver_8e1121::Session>(data) {
-            Some(m.migrate())
+    pub enum LoadResult {
+        /// `Torrent` was loaded successfully and no migration is needed.
+        Ok(Torrent),
+        /// `Torrent` was loaded successfully from an older version.
+        Migrated(Torrent),
+        /// Deserialization failed for some reason.
+        Failed,
+    }
+
+    /// `info_data` is an `Option` because older version of synapse serialized it as part of
+    /// `session_data`.
+    pub fn load(session_data: &[u8], info_data: Option<&[u8]>) -> LoadResult {
+        if let Some(info_data) = info_data {
+            if let Ok((session, info)) = bincode::deserialize::<ver_bfbf28::Session>(session_data)
+                .and_then(|session| {
+                    Ok((
+                        session,
+                        bincode::deserialize::<ver_bfbf28::Info>(info_data)?,
+                    ))
+                })
+            {
+                LoadResult::Ok(Torrent { info, session })
+            } else {
+                LoadResult::Failed
+            }
+        } else if let Ok(m) = bincode::deserialize::<ver_fa1b6f::Session>(session_data) {
+            LoadResult::Migrated(m.migrate())
+        } else if let Ok(m) = bincode::deserialize::<ver_6e27af::Session>(session_data) {
+            LoadResult::Migrated(m.migrate())
+        } else if let Ok(m) = bincode::deserialize::<ver_249b1b::Session>(session_data) {
+            LoadResult::Migrated(m.migrate())
+        } else if let Ok(m) = bincode::deserialize::<ver_5f166d::Session>(session_data) {
+            LoadResult::Migrated(m.migrate())
+        } else if let Ok(m) = bincode::deserialize::<ver_8e1121::Session>(session_data) {
+            LoadResult::Migrated(m.migrate())
         } else {
-            None
+            LoadResult::Failed
+        }
+    }
+
+    pub mod ver_bfbf28 {
+        use chrono::{DateTime, Utc};
+
+        use super::ver_fa1b6f as prev;
+        use super::Bitfield;
+
+        pub use prev::{File, Status, StatusState};
+
+        #[derive(Debug, PartialEq)]
+        pub struct Torrent {
+            pub info: Info,
+            pub session: Session,
+        }
+
+        #[derive(Deserialize, Debug, PartialEq, Serialize)]
+        pub struct Session {
+            pub announce: Option<String>,
+            pub creator: Option<String>,
+            pub comment: Option<String>,
+            pub pieces: Bitfield,
+            pub uploaded: u64,
+            pub downloaded: u64,
+            pub status: prev::Status,
+            pub path: Option<String>,
+            pub priority: u8,
+            pub priorities: Vec<u8>,
+            pub created: DateTime<Utc>,
+            pub throttle_ul: Option<i64>,
+            pub throttle_dl: Option<i64>,
+            pub trackers: Vec<String>,
+        }
+
+        #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+        /// Any data derived from the torrent's info dictionary; immutable and cannot change, since
+        /// any change in these fields would change the infohash itself.
+        pub struct Info {
+            pub name: String,
+            pub piece_len: u32,
+            pub total_len: u64,
+            pub hashes: Vec<Vec<u8>>,
+            pub hash: [u8; 20],
+            pub files: Vec<prev::File>,
+            pub private: bool,
+            pub be_name: Option<Vec<u8>>,
+            pub piece_idx: Vec<(usize, u64)>,
+        }
+
+        impl super::Torrent {
+            pub fn migrate(self) -> Self {
+                self
+            }
         }
     }
 
@@ -31,6 +109,7 @@ pub mod torrent {
         use chrono::{DateTime, Utc};
         use std::path::PathBuf;
 
+        use super::ver_bfbf28 as next;
         use super::Bitfield;
 
         #[derive(Deserialize, Debug, PartialEq, Serialize)]
@@ -89,19 +168,46 @@ pub mod torrent {
         }
 
         impl Session {
-            pub fn migrate(self) -> Self {
-                self
+            pub fn migrate(self) -> next::Torrent {
+                let session = next::Session {
+                    announce: self.info.announce,
+                    creator: self.info.creator,
+                    comment: self.info.comment,
+                    pieces: self.pieces,
+                    uploaded: self.uploaded,
+                    downloaded: self.downloaded,
+                    status: self.status,
+                    path: self.path,
+                    priority: self.priority,
+                    priorities: self.priorities,
+                    created: self.created,
+                    throttle_ul: self.throttle_ul,
+                    throttle_dl: self.throttle_dl,
+                    trackers: self.trackers,
+                };
+                let info = next::Info {
+                    name: self.info.name,
+                    piece_len: self.info.piece_len,
+                    total_len: self.info.total_len,
+                    hashes: self.info.hashes,
+                    hash: self.info.hash,
+                    files: self.info.files,
+                    private: self.info.private,
+                    be_name: self.info.be_name,
+                    piece_idx: self.info.piece_idx,
+                };
+                next::Torrent { session, info }
             }
         }
     }
 
     pub mod ver_6e27af {
-        pub use self::next::{File, Status, StatusState};
-
         use chrono::{DateTime, Utc};
 
         use super::ver_fa1b6f as next;
         use super::Bitfield;
+
+        pub use self::next::{File, Status, StatusState};
 
         #[derive(Serialize, Deserialize)]
         pub struct Session {
@@ -134,7 +240,7 @@ pub mod torrent {
         }
 
         impl Session {
-            pub fn migrate(self) -> super::current::Session {
+            pub fn migrate(self) -> super::current::Torrent {
                 next::Session {
                     info: next::Info {
                         comment: None,
@@ -168,12 +274,12 @@ pub mod torrent {
     }
 
     pub mod ver_249b1b {
-        pub use self::next::{File, Info, Status, StatusState};
-
         use chrono::{DateTime, Utc};
 
         use super::ver_6e27af as next;
         use super::Bitfield;
+
+        pub use self::next::{File, Info, Status, StatusState};
 
         #[derive(Serialize, Deserialize)]
         pub struct Session {
@@ -191,7 +297,7 @@ pub mod torrent {
         }
 
         impl Session {
-            pub fn migrate(self) -> super::current::Session {
+            pub fn migrate(self) -> super::current::Torrent {
                 let mut trackers = Vec::new();
                 if let Some(ref url) = self.info.announce {
                     trackers.push(url.to_owned());
@@ -262,7 +368,7 @@ pub mod torrent {
         }
 
         impl Session {
-            pub fn migrate(self) -> super::current::Session {
+            pub fn migrate(self) -> super::current::Torrent {
                 let mut state = next::StatusState::Complete;
                 for i in 0..self.pieces.len - 1 {
                     if !(self.pieces.data[i as usize]) != 0 {
@@ -366,7 +472,7 @@ pub mod torrent {
         }
 
         impl Session {
-            pub fn migrate(self) -> super::current::Session {
+            pub fn migrate(self) -> super::current::Torrent {
                 next::Session {
                     info: self.info,
                     pieces: self.pieces,
@@ -391,10 +497,130 @@ mod tests {
     use chrono::DateTime;
     use std::path::PathBuf;
 
-    use super::torrent::current::{File, Info, Session, Status, StatusState};
-    use super::torrent::Bitfield;
+    use super::torrent::*;
 
-    fn session_instance() -> Session {
+    #[test]
+    fn ver_bfbf28_serialize() {
+        let torrent = ver_bfbf28_torrent_instance();
+        assert_eq!(
+            bincode::serialize(&torrent.info).unwrap(),
+            VER_BFBF28_INFO_SERIALIZATION
+        );
+        assert_eq!(
+            bincode::serialize(&torrent.session).unwrap(),
+            VER_BFBF28_SESSION_SERIALIZATION
+        );
+    }
+
+    #[test]
+    fn ver_bfbf28_deserialize() {
+        // Implementation here.
+    }
+
+    #[test]
+    fn ver_bfbf28_migrate_from_ver_fa1b6f() {
+        let LoadResult::Migrated(torrent) = load(VER_FA1B6F_SESSION_SERIALIZATION, None) else {
+            panic!("expected migration");
+        };
+        assert_eq!(torrent, ver_bfbf28_torrent_instance());
+    }
+
+    #[test]
+    fn ver_fa1b6f_deserialize() {
+        assert_eq!(
+            bincode::serialize(&ver_fa1b6f_session_instance()).unwrap(),
+            VER_FA1B6F_SESSION_SERIALIZATION
+        );
+    }
+
+    #[test]
+    fn ver_fa1b6f_serialize() {
+        assert_eq!(
+            bincode::deserialize::<ver_fa1b6f::Session>(&VER_FA1B6F_SESSION_SERIALIZATION).unwrap(),
+            ver_fa1b6f_session_instance()
+        );
+    }
+
+    fn ver_bfbf28_torrent_instance() -> ver_bfbf28::Torrent {
+        use ver_bfbf28::*;
+
+        Torrent {
+            info: Info {
+                name: "Hello world!".to_string(),
+                piece_len: 1048576,
+                total_len: 2 * 1048576,
+                hashes: vec![
+                    b"\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29".to_vec(),
+                    b"\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39".to_vec(),
+                ],
+                hash: *b"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19",
+                files: vec![
+                    File {
+                        path: PathBuf::from("file1"),
+                        length: 1024,
+                    },
+                    File {
+                        path: PathBuf::from("file2"),
+                        length: 2 * 1048576 - 1024,
+                    },
+                ],
+                private: true,
+                be_name: None,
+                piece_idx: vec![(0, 1), (1, 0)],
+            },
+            session: Session {
+                announce: Some("announce".to_string()),
+                creator: Some("creator".to_string()),
+                comment: Some("comment".to_string()),
+                pieces: Bitfield {
+                    len: 2,
+                    data: Box::new([3]),
+                },
+                uploaded: 7777777,
+                downloaded: 88888888,
+                status: Status {
+                    paused: false,
+                    validating: true,
+                    error: Some("an error".to_string()),
+                    state: StatusState::Complete,
+                },
+                path: Some("/tmp".to_string()),
+                priority: 100,
+                priorities: vec![],
+                created: DateTime::from_timestamp(946684799, 0).unwrap(),
+                throttle_ul: Some(64 * 1024 * 1024),
+                throttle_dl: None,
+                trackers: vec!["https://example.com:1234/tracker".to_string()],
+            }
+        }
+    }
+
+    const VER_BFBF28_INFO_SERIALIZATION: &[u8] = &[
+        12, 0, 0, 0, 0, 0, 0, 0, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33, 0, 0, 16,
+        0, 0, 0, 32, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 32, 33, 34,
+        35, 36, 37, 38, 39, 40, 41, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 20, 0, 0, 0, 0, 0, 0,
+        0, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 16, 17,
+        18, 19, 20, 21, 22, 23, 24, 25, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 2, 0, 0, 0, 0, 0,
+        0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 102, 105, 108, 101, 49, 0, 4, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0,
+        0, 0, 0, 0, 102, 105, 108, 101, 50, 0, 252, 31, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0,
+    ];
+    const VER_BFBF28_SESSION_SERIALIZATION: &[u8] = &[
+        1, 8, 0, 0, 0, 0, 0, 0, 0, 97, 110, 110, 111, 117, 110, 99, 101, 1, 7, 0, 0, 0, 0, 0, 0, 0,
+        99, 114, 101, 97, 116, 111, 114, 1, 7, 0, 0, 0, 0, 0, 0, 0, 99, 111, 109, 109, 101, 110,
+        116, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3, 241, 173, 118, 0, 0, 0, 0, 0, 56,
+        86, 76, 5, 0, 0, 0, 0, 0, 1, 1, 8, 0, 0, 0, 0, 0, 0, 0, 97, 110, 32, 101, 114, 114, 111,
+        114, 2, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 47, 116, 109, 112, 100, 0, 0, 0, 0, 0, 0, 0, 0,
+        20, 0, 0, 0, 0, 0, 0, 0, 49, 57, 57, 57, 45, 49, 50, 45, 51, 49, 84, 50, 51, 58, 53, 57,
+        58, 53, 57, 90, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 0, 0,
+        0, 104, 116, 116, 112, 115, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111, 109,
+        58, 49, 50, 51, 52, 47, 116, 114, 97, 99, 107, 101, 114,
+    ];
+
+    fn ver_fa1b6f_session_instance() -> ver_fa1b6f::Session {
+        use ver_fa1b6f::*;
+
         Session {
             info: Info {
                 name: "Hello world!".to_string(),
@@ -444,7 +670,7 @@ mod tests {
         }
     }
 
-    const SESSION_SERIALIZATION: &[u8] = &[
+    const VER_FA1B6F_SESSION_SERIALIZATION: &[u8] = &[
         12, 0, 0, 0, 0, 0, 0, 0, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33, 1, 8, 0,
         0, 0, 0, 0, 0, 0, 97, 110, 110, 111, 117, 110, 99, 101, 1, 7, 0, 0, 0, 0, 0, 0, 0, 99, 114,
         101, 97, 116, 111, 114, 1, 7, 0, 0, 0, 0, 0, 0, 0, 99, 111, 109, 109, 101, 110, 116, 0, 0,
@@ -463,20 +689,4 @@ mod tests {
         0, 0, 0, 104, 116, 116, 112, 115, 58, 47, 47, 101, 120, 97, 109, 112, 108, 101, 46, 99,
         111, 109, 58, 49, 50, 51, 52, 47, 116, 114, 97, 99, 107, 101, 114,
     ];
-
-    #[test]
-    fn stable_deserialize() {
-        assert_eq!(
-            bincode::serialize(&session_instance()).unwrap(),
-            SESSION_SERIALIZATION
-        );
-    }
-
-    #[test]
-    fn stable_serialize() {
-        assert_eq!(
-            super::torrent::load(&SESSION_SERIALIZATION).unwrap(),
-            session_instance()
-        );
-    }
 }

src/control/mod.rs
@@ -1,4 +1,3 @@
-use std::io::Read;
 use std::net::TcpStream;
 use std::path::PathBuf;
 use std::sync::atomic;
@@ -176,7 +175,7 @@ impl<T: cio::CIO> Control<T> {
         }
         debug!("Serializing torrents!");
         for torrent in self.torrents.values_mut() {
-            torrent.serialize_if_dirty();
+            torrent.serialize_session_if_dirty();
         }
     }
 
@@ -195,7 +194,12 @@ impl<T: cio::CIO> Control<T> {
         }
 
         debug!("Deserializing torrents!");
-        for entry in fs::read_dir(sd)? {
+        // Eagerly read the list of files in the session directory; deserializing a torrent may
+        // migrate the data on disk to a new format and modify/write files to the session
+        // directory. POSIX does not provide any guarantees about how `opendir()` and `readdir()`
+        // will behave with respect to concurrent mutations.
+        let entries: Vec<_> = fs::read_dir(sd)?.collect();
+        for entry in entries {
             if self.deserialize_torrent(entry).is_err() {
                 error!(
                     "Please ensure that session data is not corrupted and not past version {}",
@@ -215,14 +219,28 @@ impl<T: cio::CIO> Control<T> {
             return Ok(());
         }
         trace!("Attempting to deserialize file {:?}", dir);
-        let mut f = fs::File::open(dir.path())?;
-        let mut data = Vec::new();
-        f.read_to_end(&mut data)?;
-        trace!("Succesfully read file");
+        let mut path = dir.path();
+        let session_data = fs::read(&path)?;
+        trace!("Succesfully read session file");
+        assert!(path.set_extension("info"));
+        let info_data = match fs::read(&path) {
+            Ok(data) => Ok(Some(data)),
+            // Older versions of synapse serialized the info as part of the session state, so a
+            // missing info file is not necessarily fatal.
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(None),
+            Err(e) => Err(e),
+        }?;
+        trace!("Successully read info file");
 
         let tid = self.tid_cnt;
         let throttle = self.throttler.get_throttle(tid);
-        if let Some(t) = Torrent::deserialize(tid, &data, throttle, self.cio.new_handle()) {
+        if let Some(t) = Torrent::deserialize(
+            tid,
+            &session_data,
+            info_data.as_deref(),
+            throttle,
+            self.cio.new_handle(),
+        ) {
             trace!("Succesfully parsed torrent file {:?}", dir.path());
             self.hash_idx.insert(t.info().hash, tid);
             self.tid_cnt += 1;

commit 6d95c48fee57567ba8c9e256afaaddfb905c0a2f

Clean up all associated files from session directory when removing torrent
Fixes a bug introduced in abcb6507d87bedc63bc721ce48d3fb8237d4f902; that
commit split per-torrent session state into two files, but forgot to
update disk::Request::Delete to cleanup the new .info file.

src/disk/job.rs
@@ -450,6 +450,8 @@ impl Request {
                     let spb = tpb.get(sd);
                     spb.push(hash_to_id(&hash));
                     fs::remove_file(&spb).ok();
+                    spb.set_extension("info");
+                    fs::remove_file(&spb).ok();
                 }
 
                 for file in &files {